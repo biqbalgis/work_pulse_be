2a04506823637a506608d9669e22e2d861deceb3
Uzair ul Hassan <uzairshah.gis@gmail.com>
2025-11-25 20:30:46 +0500

    Update Soft Delete Test Py

--- core/tests/test_soft_delete.py
+++ core/tests/test_soft_delete.py
@@ -1,31 +1,29 @@
 from django.test import TestCase
-from core.models import SoftDeleteModel
-from django.db import models
+from tags.models import Tag
+from workspaces.models import Workspace
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-# Create a dummy model for testing
-class TestModel(SoftDeleteModel):
-    name = models.CharField(max_length=100)
-    
-    class Meta:
-        app_label = 'core'
+User = get_user_model()
 
 class SoftDeleteTests(TestCase):
     def setUp(self):
-        self.obj1 = TestModel.objects.create(name="Object 1")
-        self.obj2 = TestModel.objects.create(name="Object 2")
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.workspace = Workspace.objects.create(name='Test Workspace', created_by=self.user)
+        self.obj1 = Tag.objects.create(name="Tag 1", workspace=self.workspace)
+        self.obj2 = Tag.objects.create(name="Tag 2", workspace=self.workspace)
 
     def test_soft_delete(self):
         """Test that delete() sets is_deleted=True and filters from default queryset"""
         # Initial state
-        self.assertEqual(TestModel.objects.count(), 2)
+        self.assertEqual(Tag.objects.count(), 2)
         
         # Soft delete obj1
         self.obj1.delete()
         
         # Verify it's gone from default manager
-        self.assertEqual(TestModel.objects.count(), 1)
-        self.assertEqual(TestModel.objects.first(), self.obj2)
+        self.assertEqual(Tag.objects.count(), 1)
+        self.assertEqual(Tag.objects.first(), self.obj2)
         
         # Verify it still exists in DB
         self.obj1.refresh_from_db()
@@ -37,10 +35,10 @@ class SoftDeleteTests(TestCase):
         self.obj1.delete()
         
         # Default manager excludes it
-        self.assertEqual(TestModel.objects.count(), 1)
+        self.assertEqual(Tag.objects.count(), 1)
         
         # All objects manager includes it
-        self.assertEqual(TestModel.all_objects.count(), 2)
+        self.assertEqual(Tag.all_objects.count(), 2)
 
     def test_restore(self):
         """Test that we can restore a soft-deleted item"""
@@ -53,4 +51,4 @@ class SoftDeleteTests(TestCase):
         self.obj1.save()
         
         # Verify it's back
-        self.assertEqual(TestModel.objects.count(), 2)
+        self.assertEqual(Tag.objects.count(), 2)
