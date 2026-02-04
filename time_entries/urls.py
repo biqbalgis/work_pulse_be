f2a7aa2f36123c3b7c855c6ac70c57590d10ba71
Uzair ul Hassan <uzairshah.gis@gmail.com>
2026-02-04 10:55:19 +0500

    Update Bulk Upload API and URL

--- time_entries/urls.py
+++ time_entries/urls.py
@@ -3,5 +3,5 @@ from .views import TimeEntryViewSet, BulkTimeEntryViewSet
 
 router = DefaultRouter()
 router.register(r'time-entries', TimeEntryViewSet, basename='time-entry')
-router.register(r'bulk-create', BulkTimeEntryViewSet, basename='bulk-create')
+router.register(r'time_entries/bulk-create', BulkTimeEntryViewSet, basename='bulk-create')
 urlpatterns = router.urls
