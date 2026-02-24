5feac9d26386f14f24d9bae4fbfea62c40ffc6ae
Uzair ul Hassan <uzairshah.gis@gmail.com>
2026-02-25 00:07:14 +0500

    Update Costing Report data to server

--- reports/cost_calculator.py
+++ reports/cost_calculator.py
@@ -263,7 +263,7 @@ class CostCalculator:
         
         start_t = entry.start_time.astimezone(timezone.get_current_timezone()).time()
         night_start = time(18, 0)
-        morning_start = time(5, 50)
+        morning_start = time(5, 00)
         
         is_night = (start_t >= night_start) or (start_t < morning_start)
         
