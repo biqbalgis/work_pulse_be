b857359cdaa92d15ebeb7761cde78995db1e3451
Uzair ul Hassan <uzairshah.gis@gmail.com>
2026-03-04 23:47:08 +0500

    Revert "Update the Signature with Name and Date with it."
    This reverts commit 26370e05714ca7c9dc81fcbe261e2ee8aebdb39d.

--- reports/urls.py
+++ reports/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import WeeklyPayrollReport, DailyRTOTReport, DailyDetailView, EmployeePayrollDashboard, DailyWorkReportView, \
-    LEMReportGenerationView, LEMDailyReportView, LEMCostingReportView, TimeEntryExcelReportView
+    LEMReportGenerationView, LEMDailyReportView, LEMCostingReportView
 
 urlpatterns = [
     path("weekly-payroll/", WeeklyPayrollReport.as_view(), name="weekly-payroll"),
@@ -11,5 +11,4 @@ urlpatterns = [
     path("lem-report/dailylemreport/", LEMDailyReportView.as_view(), name="lem-report-generate"),
     path("lem-report/costing/", LEMCostingReportView.as_view(), name="lem-report-costing"),
     path("payroll/employees/", EmployeePayrollDashboard.as_view()),
-    path("time-entry-excel/", TimeEntryExcelReportView.as_view(), name="time-entry-excel-report"),
 ]
