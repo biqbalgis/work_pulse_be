f003481451b072c9596d95f93e6924611146a028
Uzair ul Hassan <uzairshah.gis@gmail.com>
2026-03-27 17:13:18 +0500

    Test server Push

--- time_entries/urls.py
+++ time_entries/urls.py
@@ -5,3 +5,6 @@ router = DefaultRouter()
 router.register(r'time-entries', TimeEntryViewSet, basename='time-entry')
 router.register(r'time_entries/bulk-create', BulkTimeEntryViewSet, basename='bulk-create')
 urlpatterns = router.urls
+
+
+
