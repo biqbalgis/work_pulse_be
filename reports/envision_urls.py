d2bce770f6220172bcb9cc947fa71a1d8cd79097
Uzair ul Hassan <uzairshah.gis@gmail.com>
2026-06-14 16:15:52 +0500

    Weekly update the report PM info

--- reports/envision_urls.py
+++ reports/envision_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .envision_views import EnvisionLEMReportView, EnvisionLEMSearchView
+from .envision_timesheet_views import EnvisionTimesheetReportView
 
 urlpatterns = [
     path("fieldTicket_Lem/", EnvisionLEMReportView.as_view(), name="envision-field-ticket-lem"),
     path("lem/search/", EnvisionLEMSearchView.as_view(), name="envision-lem-search"),
+    path("timesheet/", EnvisionTimesheetReportView.as_view(), name="envision-timesheet"),
 ]
